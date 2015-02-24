0992e8558099dfa64db0d7d69d4097aa42668506
Ben Pritchard and Tucker Mayo <tuckerjamesmayo@gmail.com>
2015-02-24 13:52:17 -0800

    finalize logic for restaurant app

--- services/OrdersFactory.js
+++ services/OrdersFactory.js
@@ -2,26 +2,35 @@ pizzaCream.factory('OrdersFactory', function OrdersFactory() {
   var factory = {};
   factory.slices = 0;
   factory.scoops = 0;
+  factory.slicesAmount = 0;
+  factory.scoopsAmount = 0;
+
 
   factory.addSlice = function() {
     factory.slices += 1;
+    factory.slicesAmount += 3;
   };
 
+
   factory.addScoop = function() {
-    var scoop = { amount: factory.scoopAmount };
-    factory.scoops.push(scoop);
-    factory.scoopAmount = null;
+    factory.scoops += 1;
+    factory.scoopsAmount += 3;
   };
 
   factory.deleteSlice = function(slice) {
     if (factory.slices > 0) {
       factory.slices -= 1;
+      factory.slicesAmount -= 3;
     }
+
   };
 
   factory.deleteScoop = function(scoop) {
-    var index = factory.scoops.indexOf(scoop)
-    factory.scoops.splice(index, 1);
+    if (factory.scoops > 0) {
+      factory.scoops -= 1;
+      factory.scoopsAmount -= 3;
+
+    }
   };
   return factory;
 });
